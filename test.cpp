0bdbce86f356b6cc999c212ac96be9887e23de1a
Oli <oliver@fraclog.co>
2020-07-13 22:29:08 +0100

    simpler

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+#include "FeatherFifo.hpp"
+
+SCENARIO("FeatherFifo acts as a small fifo ring-buffer", "[fifo][ring buffer]")
+{
+  enum TSample
+  {
+    A,
+    B,
+    C
+  };
+  GIVEN("An empty FeatherFifo")
+  {
+    FeatherFifo<> ff;
+
+    REQUIRE(ff.empty());
+  }
+}
